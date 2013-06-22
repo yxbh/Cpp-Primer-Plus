648ac2ccf4a7d5e1c6eb3b3fc4df90e611f0b53b
Ben <hhyyxx008@hotmail.com>
2013-06-22 11:37:34 +0800

    (Chapter 18) : headers

---
+++ src/Chapter 18/chapter18_exercises.h
@@ -0,0 +1,9 @@
+#ifndef _CHAPTER18_EXERCISES_H_
+#define _CHAPTER18_EXERCISES_H_
+
+extern void ch18ex1(void);
+extern void ch18ex2(void);
+extern void ch18ex3(void);
+extern void ch18ex4(void);
+
+#endif
---
+++ src/Chapter 18/chapter18_review.h
@@ -0,0 +1,5 @@
+#ifndef _CHAPTER18_REVIEW_H_
+#define _CHAPTER18_REVIEW_H_
+
+
+#endif
