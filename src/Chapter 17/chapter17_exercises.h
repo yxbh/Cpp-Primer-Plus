a2bbb30e75424e8da618eb078e24cda1f6b85894
Ben <hhyyxx008@hotmail.com>
2013-06-22 11:37:28 +0800

    (Chapter 17) : headers

---
+++ src/Chapter 17/chapter17_exercises.h
@@ -0,0 +1,11 @@
+#ifndef _CHAPTER17_EXERCISES_H_
+#define _CHAPTER17_EXERCISES_H_
+
+extern void ch17ex1(void);
+extern void ch17ex2(void);
+extern void ch17ex3(void);
+extern void ch17ex4(void);
+extern void ch17ex5(void);
+extern void ch17ex6(void);
+
+#endif
---
+++ src/Chapter 17/chapter17_review.h
@@ -0,0 +1,5 @@
+#ifndef _CHAPTER17_REVIEW_H_
+#define _CHAPTER17_REVIEW_H_
+
+
+#endif
