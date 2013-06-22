ca83d681eccf4ab40e37590551adbcb138a903d0
Ben <hhyyxx008@hotmail.com>
2013-06-22 11:47:26 +0800

    (Chapter 15) : misc

--- src/Chapter 15/chapter15_exercises.h
+++ src/Chapter 15/chapter15_exercises.h
@@ -12,4 +12,9 @@ extern void ch15ex4(void);
 extern int error4(void);
 extern int use_sales(void);
 
+//ch15ex1();
+	//ch15ex2();
+	//ch15ex3(); // calls the same function as ch15ex2()
+	//ch15ex4();
+
 #endif
