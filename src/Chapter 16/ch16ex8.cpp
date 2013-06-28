60150e74c0f44f681b857439d01a549098987eb5
Ben <hhyyxx008@hotmail.com>
2013-06-28 21:56:21 +0800

    (Chapter 16) : progress update

--- src/Chapter 16/ch16ex8.cpp
+++ src/Chapter 16/ch16ex8.cpp
@@ -1,6 +1,26 @@
 #include "chapter16_exercises.h"
 
+typedef std::vector<std::string> nameList;
+
+static nameList GetNames(void);
+static nameList Merge(nameList list1, nameList list2);
+
 void ch16ex8(void)
+{
+	// get first name list
+
+	// get second name list
+
+	// merge the two and display
+
+}
+
+nameList GetNames(void)
+{
+
+}
+
+nameList Merge(nameList list1, nameList list2)
 {
 
 }
