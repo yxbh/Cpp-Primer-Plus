7a7403e15b7bf12465e6aa6b5cb33fbc7ecca694
Ben <hhyyxx008@hotmail.com>
2013-06-28 21:15:26 +0800

    (Chapter 16) : progress update
    exercise 4

--- src/Chapter 16/ch16ex4.cpp
+++ src/Chapter 16/ch16ex4.cpp
@@ -1,6 +1,19 @@
 #include "chapter16_exercises.h"
 
+static int reduce(long ar[], int n);
+
 void ch16ex4(void)
 {
+	long ar[] = {32, 12, 123, 54, 123, 12};
 
+	std::cout << std::endl << reduce(ar, 6) << std::endl;
+}
+
+int reduce(long ar[], int n)
+{
+
+	std::sort(ar, ar+n);
+	auto ptr = std::unique(ar, ar+n);
+	std::copy(ar, ptr, std::ostream_iterator<long>(std::cout, " "));
+	return ptr-ar;
 }
